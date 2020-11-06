5001830fef5ae44d57b2ffaa8909f74697095fda
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-11-06 23:36:01 +0100

    add base truth data model

---
+++ Assets/Scripts/Installers/DataModelsInstaller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class DataModelsInstaller : MonoInstaller
+{
+    [SerializeField] private TruthDataSO _truthData = default;
+    [SerializeField] private TruthDataModel _truthDataModelPrefab = default;
+    
+    public override void InstallBindings()
+    {
+        Container.Bind<TruthDataModel>().FromComponentInNewPrefab(_truthDataModelPrefab).AsSingle();
+    }
+}
---
+++ Assets/Scripts/SO/TruthDataSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/TruthDataSO")]
+public class TruthDataSO : ScriptableObject
+{
+    [SerializeField] private float _defaultTruthAmount = default;
+    
+    [Space]
+    [SerializeField] private float _baseTruthReducePerSecond = default;
+    
+    public float defaultTruthAmount => _defaultTruthAmount;
+    public float baseTruthReducePerSecond => _baseTruthReducePerSecond;
+}
