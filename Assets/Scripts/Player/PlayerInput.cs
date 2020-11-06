01933190b044f2cd1ec4288a9d29e525bd0c16c1
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-11-06 22:53:00 +0100

    add player input and movement

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private float smoothTime;
+    
+    public Vector2 movementInput { get; private set; }
+    
+    private PlayerInputActions _playerInputActions;
+    private Vector2 _inputVelocity;
+    
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+    }
+    
+    private void OnEnable()
+    {
+        _playerInputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInputActions.Disable();
+    }
+
+    void Update()
+    {
+        var target = _playerInputActions.Player.Movement.ReadValue<Vector2>();
+
+        var x = Mathf.SmoothDamp(movementInput.x, target.x, ref _inputVelocity.x, smoothTime);
+        var y = Mathf.SmoothDamp(movementInput.y, target.y, ref _inputVelocity.y, smoothTime);
+        
+        movementInput = new Vector2(x, y);
+    }
+}
