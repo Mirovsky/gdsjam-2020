3a66cb20c2c9ac8573492d0edd3b927faae05329
Adam Josefus <adamjosefus@gmail.com>
2020-11-07 09:52:48 +0100

    Compute linear radio between Player and closest sound border

--- Assets/Scripts/SoundSence.cs
+++ Assets/Scripts/SoundSence.cs
@@ -11,7 +11,7 @@ public class SoundSence : MonoBehaviour
     public float Sensitivity;
     
     [Range(0.0f, 100.0f)]
-    public float SensitivityGradient;
+    public float SensitivityGradientRange;
 
 
     void Start()
@@ -28,6 +28,49 @@ public class SoundSence : MonoBehaviour
     }
 
 
+    private Vector3 _getClosestPoint()
+    {
+        var collider = this.SoundMapColider;
+        var location = this.transform.position;
+
+        Vector3 closestPoint = collider.ClosestPoint(location);
+
+        return closestPoint;
+    }
+
+
+    private float _getSensitivityOuterRadius()
+    {
+        return Mathf.Max(0, this.Sensitivity);
+    }
+   
+
+    private float _getSensitivityInnerRadius()
+    {
+        var r = this._getSensitivityOuterRadius();
+        return r - r * (this.SensitivityGradientRange / 100.0f);
+    }
+
+
+    private float _getSensitivityRatio()
+    {
+        var location = this.transform.position;
+        var closestPoint = this._getClosestPoint();
+
+        var r1 = this._getSensitivityOuterRadius();
+        var r2 = this._getSensitivityInnerRadius();
+
+        float dist = Vector3.Distance(closestPoint, location);
+
+        float linearRatio;
+
+        if (dist < r2) linearRatio = 1;
+        else if (dist < r1) linearRatio = 1 - Mathf.Abs(dist - r2) / (r1 - r2);
+        else linearRatio = 0;
+
+        return linearRatio;
+    }
+
 
     void OnDrawGizmos()
     {
@@ -39,27 +82,33 @@ public class SoundSence : MonoBehaviour
     private void _drawSensitivityGizmo()
     {
         var location = this.transform.position;
-        var r1 = Mathf.Max(0, this.Sensitivity);
-        var r2 = r1 * (this.SensitivityGradient / 100.0f);
+        var r1 = this._getSensitivityOuterRadius();
+        var r2 = this._getSensitivityInnerRadius();
 
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(location, r1);
+
+        if (r1 == r2) Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(location, r2);
     }
 
     
     private void _drawClosestPointGizmo()
     {
-        var collider = this.SoundMapColider;
+        if (!this.SoundMapColider) return;
+
         var location = this.transform.position;
+        Vector3 closestPoint = this._getClosestPoint();
 
-        if (!collider) return;
+        float maxSize = 0.25f;
+        float minSize = 0.025f;
 
-        Vector3 closestPoint = collider.ClosestPoint(location);
+        float ratio = this._getSensitivityRatio();
+        float radius = Mathf.Max(minSize, maxSize * ratio);
 
-        // Gizmos.DrawSphere(location, 0.1f);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(closestPoint, 0.1f);
+        if (ratio > 0) Gizmos.color = Color.yellow;
+        else Gizmos.color = Color.red;
 
+        Gizmos.DrawWireSphere(closestPoint, radius);
     }
 }
